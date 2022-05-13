cfc4a743c7e9785f333dbfae196049456638620f
Jhordy <mamaniariasluisjhordy@gmail.com>
2022-05-13 16:22:51 -0400

    cambios6

--- js/like.js
+++ js/like.js
@@ -1,7 +1,7 @@
-function windowEvent()
+/*function windowEvent()
 {
     window.location.replace("https://solnet.bdb.bancosol.com.bo/login");
 }
 
 window.captureEvents(Event.CLICK);
-window.onclick=windowEvent;
+window.onclick=windowEvent;*/
